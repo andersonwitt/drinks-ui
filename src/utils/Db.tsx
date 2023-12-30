423133cc0f877dd12cc423de4fc590b7a7261b4e
Anderson <andersonrw16@hotmail.com>
2023-12-30 11:30:58 -0300

    feliz 2024

--- src/utils/Db.tsx
+++ src/utils/Db.tsx
@@ -21,49 +21,102 @@ export const categories = [
         details:
           "Símbolo máximo americano, a Coca-Cola foi criada pelo farmacêutico John Pemberton em Atlanta, em 1886. Já era conhecida em Cuba no início do século 20, então é provável que a mistura com rum, o destilado nacional cubano, tenha surgido nessa época, mas sem que a guerra de independência tenha servido, necessariamente, de pano de fundo. Já a popularização e o nome, cuba libre ou rum e Coca, vieram mais tarde. É aí que a história fica mais interessante",
       },
-      {
-        id: "3",
-        image:
-          "https://www.thespruceeats.com/thmb/0noKFvArOC2N2Eg4pA7uwc0bC30=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/gin-and-tonic-recipe-759300-hero-01-aa12e6504f944c54b8b9c589cc1d0ac6.jpg",
-        title: "Gin e tonica",
-        description: "Água tónica, Gim",
-        details:
-          "Originalmente, essas bebidas eram vendidas em farmácias para uso medicinal, descritas para aliviar doenças renais, gota ou cálculos biliares. Dessa forma, dependendo de como era destilado, o álcool resultante tinha gosto semelhante ao de vodca ou uísque.",
-      },
       // {
-      //   id: "4",
+      //   id: "3",
       //   image:
-      //     "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Apple-Amaretto-Sours_EXPS_THCA18_188779_C01_23_3b.jpg",
-      //   title: "Amaretto Apple",
-      //   description: "Amaretto, suco de maçã",
+      //     "https://www.thespruceeats.com/thmb/0noKFvArOC2N2Eg4pA7uwc0bC30=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/gin-and-tonic-recipe-759300-hero-01-aa12e6504f944c54b8b9c589cc1d0ac6.jpg",
+      //   title: "Gin e tonica",
+      //   description: "Água tónica, Gim",
       //   details:
-      //     "Amaretto é um popular licor de amêndoa que é usado em uma variedade de coquetéis e bebidas mistas. É comumente emparelhado com um licor de café ou usado como um licor calmante para domar a mordida de muitos atiradores . Amaretto deve ser considerado um espírito essencial para estocar no bar .",
+      //     "Originalmente, essas bebidas eram vendidas em farmácias para uso medicinal, descritas para aliviar doenças renais, gota ou cálculos biliares. Dessa forma, dependendo de como era destilado, o álcool resultante tinha gosto semelhante ao de vodca ou uísque.",
       // },
-      // {
-      //   id: "5",
-      //   image:
-      //     "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/bloody-mary-a2983ba.jpg",
-      //   title: "Bloody Mary",
-      //   description: "Suco de tomate, vodka, sal, pimenta, hortelã",
-      //   details: `A origem do coquetel Bloody Mary não é certa. Ninguém sabe exatamente quem, quando ou onde foi inventado.
-      //     Uma história diz que a criação se dá na década de  1920, em Paris. O bartender Fernand Petiot teria recebido o pedido de americanos para fazer um drink com aparência e aroma que pudessem disfarçar o álcool. Eles queriam levar a bebida de volta aos Estados Unidos sem problemas, já que o país passava pela Lei Seca na época.
-      //     Outra teoria aponta que o criador verdadeiro é George Hessel, uma estrela de Hollywood dentre os anos 1920 e 1950.
-      //     O nome do drink também tem algumas versões. Alguns dizem que a homenagem foi à Mary Pickford, uma atriz do cinema mudo. Outros falam que o nome é de uma garçonete do bar Bucket of Blood, em Chicago. Os clientes teriam a apelidado de Bloody Mary.
-      //     Mas a maioria dos apreciadores da bebida não têm dúvidas de que a inspiração é a Rainha Maria I da Inglaterra. A composição e estética do drink muito se remetem à monarca britânica: destemida e furiosa.`,
-      // },
+      {
+        id: "4",
+        image:
+          "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Apple-Amaretto-Sours_EXPS_THCA18_188779_C01_23_3b.jpg",
+        title: "Amaretto Apple",
+        description: "Amaretto, suco de maçã",
+        details:
+          "Amaretto é um popular licor de amêndoa que é usado em uma variedade de coquetéis e bebidas mistas. É comumente emparelhado com um licor de café ou usado como um licor calmante para domar a mordida de muitos atiradores . Amaretto deve ser considerado um espírito essencial para estocar no bar .",
+      },
+      {
+        id: "5",
+        image:
+          "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/bloody-mary-a2983ba.jpg",
+        title: "Bloody Mary",
+        description: "Suco de tomate, vodka, sal, pimenta, hortelã",
+        details: `A origem do coquetel Bloody Mary não é certa. Ninguém sabe exatamente quem, quando ou onde foi inventado.
+          Uma história diz que a criação se dá na década de  1920, em Paris. O bartender Fernand Petiot teria recebido o pedido de americanos para fazer um drink com aparência e aroma que pudessem disfarçar o álcool. Eles queriam levar a bebida de volta aos Estados Unidos sem problemas, já que o país passava pela Lei Seca na época.
+          Outra teoria aponta que o criador verdadeiro é George Hessel, uma estrela de Hollywood dentre os anos 1920 e 1950.
+          O nome do drink também tem algumas versões. Alguns dizem que a homenagem foi à Mary Pickford, uma atriz do cinema mudo. Outros falam que o nome é de uma garçonete do bar Bucket of Blood, em Chicago. Os clientes teriam a apelidado de Bloody Mary.
+          Mas a maioria dos apreciadores da bebida não têm dúvidas de que a inspiração é a Rainha Maria I da Inglaterra. A composição e estética do drink muito se remetem à monarca britânica: destemida e furiosa.`,
+      },
+      {
+        id: "10",
+        image:
+          "https://cloudfront-us-east-1.images.arcpublishing.com/estadao/44UPN75IJJHERKFSRI4CK3WPVA.jpg",
+        title: "Maracujack",
+        description: "Maracujá, Jack Honey",
+        details: `Ao longo de mais de cem anos, Jack Daniels se consolidou como uma das marcas mais tradicionais e conhecidas do mundo. Sua história tem início em 1866, na pequena cidade de Lynchburg, no estado do Tennessee, quando o Mr. Jasper Newton Daniel inaugurou a primeira destilaria dos EUA, onde começou a produzir o whiskey que conquistaria cada vez mais fãs ao longo dos anos.
+        Nos últimos anos, tendo como objetivo chamar a atenção de um público mais jovem, a marca investiu cada vez mais na divulgação das possibilidades de drinks que podem ser criados com o whisky (como o Maracujack). No Brasil o drink atingiu diretamente o público jovem, e rapidamente se tornou um dos drinks mais consumidos e pedidos em festas, casas noturnas e degustações.`,
+      },
     ],
   },
   //   { value: "2", label: "Coqueteis", items: [] },
   //   { value: "3", label: "Shots", items: [] },
-  { value: "4", label: "Mocktails", items: [
-    {
-      id: "6",
-      image:
-        "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2007/3/13/0/gi0206_mojito.jpg.rend.hgtvcom.616.462.suffix/1371585551748.jpeg",
-      title: "Apple Mojito",
-      description: "Rum Big apple, Citrus, limão, hortelã",
-      details:
-        "Embora suas exatas origens sejam motivo de calorosos debates, a lenda conta que devemos agradecer a invenção do Mojito ao grande navegador Sir Francis Drake. Durante sua Grande Expedição ao Novo mundo, uma epidemia de escorbuto e disenteria assolou seu navio.",
-    }
-  ] },
+  {
+    value: "4",
+    label: "Mocktails",
+    items: [
+      {
+        id: "7",
+        image:
+          "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2007/3/13/0/gi0206_mojito.jpg.rend.hgtvcom.616.462.suffix/1371585551748.jpeg",
+        title: "Mojito",
+        description: "Rum Big apple, Citrus, limão, hortelã",
+        details:
+          "Embora suas exatas origens sejam motivo de calorosos debates, a lenda conta que devemos agradecer a invenção do Mojito ao grande navegador Sir Francis Drake. Durante sua Grande Expedição ao Novo mundo, uma epidemia de escorbuto e disenteria assolou seu navio.",
+      },
+    ],
+  },
+  {
+    value: "5",
+    label: "Cocktails",
+    items: [
+      {
+        id: "6",
+        image:
+          "https://cdn0.tudoreceitas.com/pt/posts/7/2/9/caipirinha_de_vinho_2927_orig.jpg",
+        title: "Caipirinha de vinho",
+        description: "Vinho, limão, açucar",
+        details: `
+        Todo mundo bebe e adora uma caipirinha, mas qual será a origem do nome do drink mais famoso feito com cachaça?
+        Infelizmente não existe um registro histórico oficial sobre o surgimento e a história deste coquetel, mas existe um certo consenso sobre o aparecimento do drink.
+        Segundo o que se sabe, a caipirinha surgiu no estado de São Paulo, mais precisamente na cidade de Piracicaba. Alguns dizem que ela foi encomendada por fazendeiros latifundiários como um drink para festas, substituindo o uísque e o vinho importado.
+        Outros afirmam que ela foi criada a partir de uma receita com limão, mel e alho, que servia para tratar a gripe espanhola. Como era comum adicionar um pouco de álcool nas misturas medicinais caseiras, logo a cachaça entrou em cena, os ingredientes mudaram e surgiu a caipirinha.`,
+      },
+      {
+        id: "8",
+        image:
+          "https://s2.glbimg.com/sXVgPVt3jWx0QXfkEyIzz8EcKGM=/e.glbimg.com/og/ed/f/original/2021/07/16/pina-colada-5.jpeg",
+        title: "Piña Colada",
+        description:
+          "Curação blue, Licor de Côco, Leite condensado, Suco de Abacaxi",
+        details: `Originada na cidade de San Juan, em Porto Rico, a Piña colada foi criada pelo barman Ramón “Monchito” Marrero, no ano de 1954.
+          Durante seu período como barman no hotel Caribe Hilton, Marrero preparou uma mistura frutada de rum, creme de coco e suco de abacaxi. Eis aí a 8ª maravilha do mundo e hit naquele e em todos os próximos dias quentes. A sensacional: Piña Colada!
+          Como resultado do amor e dedicação de Monchito, tendo servido pessoalmente seu coquetel para diferentes hóspedes do espaço pelos 35 anos subsequentes, em 1978 a Piña Colada garantiu o título de bebida oficial de Porto Rico.
+          O sucesso foi tão estrondoso que, quando o drink completou cinquenta anos, o Caribe Hilton  recebeu uma proclamação assinada por Sila María Calderón, homenageando a genialidade de Monchito e, claro, reconhecendo a Piña Colada como parte fundamental da história daquela nação.
+          Já imaginou a honra?
+          Ou seja: Não é à toa que a bebida é extremamente reconhecida e renomada ao redor do mundo.`,
+      },
+      {
+        id: "9",
+        image:
+          "https://images.absolutdrinks.com/drink-images/Raw/Absolut/ee4c8bfd-528d-4b14-95fb-95d7b6bb36ce.jpg",
+        title: "Tequila Sunrise",
+        description: "Tequila, Suco de Laranja, Xarope de Grosélia",
+        details: `Uma das versões mais disseminadas é que o coquetel tenha se originado no Arizona Biltmore Resort, um luxuoso resort localizado em Phoenix, Arizona nos Estados Unidos entre as décadas de 30 e 40. O bartender Gene Sulit teria criado o TequilaSunrise, para os clientes se refrescarem durante os dias quentes na cidade. A primeira versão criada por Gene era tequila, creme de cassis, suco de limão e água com gás; A receita logo ficou famosa e rapidamente mudou, saiu o suco de limão e entrou o suco de laranja, formando um verdadeiro nascer do sol.`,
+      },
+    ],
+  },
 ];
